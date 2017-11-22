ddffdf39af1b07ab6a0471f247e2a740f10ed6ad
jsiu <john,siu@cookmedical.com>
2017-11-22 11:43:07 +0800

    todo example

--- src/reducers/count.js
+++
@@ -1,9 +0,0 @@
-export default function count(state = { value: 123 }, action) {
-  const value = state.value
-  switch (action.type) {
-    case 'increase':
-      return { value: value + 1 }
-    default:
-      return state
-  }
-}
--- src/reducers/minus.js
+++
@@ -1,9 +0,0 @@
-export default function minus(state = { value: 0 }, action) {
-  const value = state.value
-  switch (action.type) {
-    case 'minus':
-      return { value: value - 1 }
-    default:
-      return state
-  }
-}
