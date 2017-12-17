9665214a5356e1afc88451e26a3e207a39604b08
sbk201 <pp00p@yahoo.com.hk>
2017-12-17 22:36:24 +0800

    fix

--- src/containers/SelectionContainer.js
+++ src/containers/SelectionContainer.js
@@ -1,34 +1,42 @@
-import { connect } from 'react-redux'
-import { newQuestion,addRecord } from '../actions'
-import Selection from '../components/Selection'
+import { connect } from "react-redux";
+import { newQuestion,addRecord } from "../actions";
+import Selection from "../components/Selection";
 
 
 const mapStateToProps = (state) => {
-  return {
-    question:state.question,
-    recordAt:state.recordAt,
-  }
-}
+	return {
+		question:state.question,
+		recordAt:state.recordAt,
+	};
+};
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    newQuestion:({time,question})=>{
-      dispatch(newQuestion());
-      dispatch(addRecord(time,question));
-    },
-    testFn:()=>{
-      window.loop(5)(()=>{
-      const answer=window.store.getState().question.answer;
-      dispatch(addRecord(new Date(),answer));
-      dispatch(newQuestion());
-      })
-    },
-  }
-}
+	return {
+		newQuestion:({time,question})=>{
+			dispatch(newQuestion());
+			dispatch(addRecord(time,question));
+		},
+		testFn:(e)=>{
+			switch (e.key){
+      case 'F2': 
+        window.loop(2)(()=>{
+          const answer=window.store.getState().question.answer;
+          dispatch(addRecord(new Date(),answer));
+          dispatch(newQuestion());
+        });
+        return;
+			case 'F3': 
+        console.log('f3')
+				return;
+			default:return;
+			}
+		},
+	};
+};
 
 const SelectionContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Selection)
+	mapStateToProps,
+	mapDispatchToProps
+)(Selection);
 
-export default SelectionContainer
+export default SelectionContainer;
