6fa8055cbdc5bbcb71a6cfb5b988a71426c9a134
sbk201 <pp00p@yahoo.com.hk>
2017-12-18 21:51:22 +0800

    local.js for local setting

--- src/initData.js
+++ src/initData.js
@@ -1,20 +1,22 @@
- export default 
- {question:{ask:'',answer:''},records:[]}
+import localSetting from "./local";
+// console.log(localSetting);
 
-const questionList=[
-{full:"One",short:"1",index:"one"},
-{full:"Two",short:"2",index:"one"},
-{full:"Three",short:"3",index:"one"},
-{full:"Four",short:"4",index:"one"},
-{full:"Five",short:"5",index:"one"},
-{full:"Six",short:"6",index:"two"},
-{full:"Seven",short:"7",index:"two"},
-{full:"Eight",short:"8",index:"two"},
-{full:"Nine",short:"9",index:"two"},
-{full:"Ten",short:"10",index:"two"},
-].slice(0,5)
-// const questionList=[
-// {full:"創世記",short:"創",index:"one"}, {full:"出埃及記",short:"出",index:"one"}, {full:"利未記",short:"利",index:"one"}, {full:"民數記",short:"民",index:"one"}, {full:"申命記",short:"申",index:"one"}, {full:"約書亞記",short:"書",index:"one"}, {full:"士師記",short:"士",index:"one"}, {full:"路得記",short:"得",index:"one"}, {full:"撒母耳記上",short:"撒上",index:"one"}, {full:"撒母耳記下",short:"撒下",index:"one"}, {full:"列王紀上",short:"王上",index:"one"}, {full:"列王紀下",short:"王下",index:"one"}, {full:"歷代志上",short:"代上",index:"one"}, {full:"歷代志下",short:"代下",index:"one"}, {full:"以斯拉記",short:"拉",index:"one"}, {full:"尼希米記",short:"尼",index:"one"}, {full:"以斯帖記",short:"斯",index:"one"}, {full:"約伯記",short:"伯",index:"one"}, {full:"詩篇",short:"詩",index:"one"}, {full:"箴言",short:"箴",index:"one"}, {full:"傳道書",short:"傳",index:"one"}, {full:"雅歌",short:"歌",index:"one"}, {full:"以賽亞書",short:"賽",index:"one"}, {full:"耶利米書",short:"耶",index:"one"}, {full:"耶利米哀歌",short:"哀",index:"one"}, {full:"以西結書",short:"結",index:"one"}, {full:"但以理書",short:"但",index:"one"}, {full:"何西阿書",short:"何",index:"one"}, {full:"約珥書",short:"珥",index:"one"}, {full:"阿摩司書",short:"摩",index:"one"}, {full:"俄巴底亞書",short:"俄",index:"one"}, {full:"約拿書",short:"拿",index:"one"}, {full:"彌迦書",short:"彌",index:"one"}, {full:"那鴻書",short:"鴻",index:"one"}, {full:"哈巴谷書",short:"哈",index:"one"}, {full:"西番雅書",short:"番",index:"one"}, {full:"哈該書",short:"該",index:"one"}, {full:"撒迦利亞書",short:"亞",index:"one"}, {full:"瑪拉基書",short:"瑪",index:"one"}, {full:"馬太福音",short:"太",index:"two"}, {full:"馬可福音",short:"可",index:"two"}, {full:"路加福音",short:"路",index:"two"}, {full:"約翰福音",short:"約",index:"two"}, {full:"使徒行傳",short:"使",index:"two"}, {full:"羅馬書",short:"羅",index:"two"}, {full:"哥林多前書",short:"林前",index:"two"}, {full:"哥林多後書",short:"林後",index:"two"}, {full:"加拉太書",short:"加",index:"two"}, {full:"以弗所書",short:"弗",index:"two"}, {full:"腓立比書",short:"腓",index:"two"}, {full:"歌羅西書",short:"西",index:"two"}, {full:"帖撒羅尼迦前書",short:"帖前",index:"two"}, {full:"帖撒羅尼迦後書",short:"帖後",index:"two"}, {full:"提摩太前書",short:"提前",index:"two"}, {full:"提摩太后書",short:"提後",index:"two"}, {full:"提多書",short:"多",index:"two"}, {full:"腓利門書",short:"門",index:"two"}, {full:"希伯來書",short:"來",index:"two"}, {full:"雅各書",short:"雅",index:"two"}, {full:"彼得前書",short:"彼前",index:"two"}, {full:"彼得後書",short:"彼後",index:"two"}, {full:"約翰一書",short:"約一",index:"two"}, {full:"約翰二書",short:"約二",index:"two"}, {full:"約翰三書",short:"約三",index:"two"}, {full:"猶大書",short:"猶",index:"two"}, {full:"啟示錄",short:"啟",index:"two"},
-// ]
-// .slice(0,5)
-export {questionList}
+const questionList1=[
+	{full:"創世記",short:"創",index:"one"}, {full:"出埃及記",short:"出",index:"one"}, {full:"利未記",short:"利",index:"one"}, {full:"民數記",short:"民",index:"one"}, {full:"申命記",short:"申",index:"one"}, {full:"約書亞記",short:"書",index:"one"}, {full:"士師記",short:"士",index:"one"}, {full:"路得記",short:"得",index:"one"}, {full:"撒母耳記上",short:"撒上",index:"one"}, {full:"撒母耳記下",short:"撒下",index:"one"}, {full:"列王紀上",short:"王上",index:"one"}, {full:"列王紀下",short:"王下",index:"one"}, {full:"歷代志上",short:"代上",index:"one"}, {full:"歷代志下",short:"代下",index:"one"}, {full:"以斯拉記",short:"拉",index:"one"}, {full:"尼希米記",short:"尼",index:"one"}, {full:"以斯帖記",short:"斯",index:"one"}, {full:"約伯記",short:"伯",index:"one"}, {full:"詩篇",short:"詩",index:"one"}, {full:"箴言",short:"箴",index:"one"}, {full:"傳道書",short:"傳",index:"one"}, {full:"雅歌",short:"歌",index:"one"}, {full:"以賽亞書",short:"賽",index:"one"}, {full:"耶利米書",short:"耶",index:"one"}, {full:"耶利米哀歌",short:"哀",index:"one"}, {full:"以西結書",short:"結",index:"one"}, {full:"但以理書",short:"但",index:"one"}, {full:"何西阿書",short:"何",index:"one"}, {full:"約珥書",short:"珥",index:"one"}, {full:"阿摩司書",short:"摩",index:"one"}, {full:"俄巴底亞書",short:"俄",index:"one"}, {full:"約拿書",short:"拿",index:"one"}, {full:"彌迦書",short:"彌",index:"one"}, {full:"那鴻書",short:"鴻",index:"one"}, {full:"哈巴谷書",short:"哈",index:"one"}, {full:"西番雅書",short:"番",index:"one"}, {full:"哈該書",short:"該",index:"one"}, {full:"撒迦利亞書",short:"亞",index:"one"}, {full:"瑪拉基書",short:"瑪",index:"one"}, {full:"馬太福音",short:"太",index:"two"}, {full:"馬可福音",short:"可",index:"two"}, {full:"路加福音",short:"路",index:"two"}, {full:"約翰福音",short:"約",index:"two"}, {full:"使徒行傳",short:"使",index:"two"}, {full:"羅馬書",short:"羅",index:"two"}, {full:"哥林多前書",short:"林前",index:"two"}, {full:"哥林多後書",short:"林後",index:"two"}, {full:"加拉太書",short:"加",index:"two"}, {full:"以弗所書",short:"弗",index:"two"}, {full:"腓立比書",short:"腓",index:"two"}, {full:"歌羅西書",short:"西",index:"two"}, {full:"帖撒羅尼迦前書",short:"帖前",index:"two"}, {full:"帖撒羅尼迦後書",short:"帖後",index:"two"}, {full:"提摩太前書",short:"提前",index:"two"}, {full:"提摩太后書",short:"提後",index:"two"}, {full:"提多書",short:"多",index:"two"}, {full:"腓利門書",short:"門",index:"two"}, {full:"希伯來書",short:"來",index:"two"}, {full:"雅各書",short:"雅",index:"two"}, {full:"彼得前書",short:"彼前",index:"two"}, {full:"彼得後書",short:"彼後",index:"two"}, {full:"約翰一書",short:"約一",index:"two"}, {full:"約翰二書",short:"約二",index:"two"}, {full:"約翰三書",short:"約三",index:"two"}, {full:"猶大書",short:"猶",index:"two"}, {full:"啟示錄",short:"啟",index:"two"},
+].slice(0,5);
+const questionList2=[
+	{full:"One",short:"1",index:"one"},
+	{full:"Two",short:"2",index:"one"},
+	{full:"Three",short:"3",index:"one"},
+	{full:"Four",short:"4",index:"one"},
+	{full:"Five",short:"5",index:"one"},
+	{full:"Six",short:"6",index:"two"},
+	{full:"Seven",short:"7",index:"two"},
+	{full:"Eight",short:"8",index:"two"},
+	{full:"Nine",short:"9",index:"two"},
+	{full:"Ten",short:"10",index:"two"},
+].slice(0,5);
+const questionList= localSetting.show ? questionList1 : questionList2;
+export {questionList};
+
+export default {question:{ask:"",answer:""},records:[]};
