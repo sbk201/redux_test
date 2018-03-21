3419142a7dcd72e99f4b293d48d95c498ac40dec
sbk201 <pp00p@yahoo.com.hk>
2018-03-21 22:47:44 +0800

    copied from customer_rep

--- src/containers/AllocateContainer.js
+++ src/containers/AllocateContainer.js
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux'
 import { updateUI,selectEmp } from '../Actions.js'
 import Allocate from '../components/Allocate'
-import {uniqueArrKey} from '../init/global';
+import { mergeClone,uniqueArrKey } from '../init/global';
 const contName="Allocate";
 const mapStateToProps = (state) => {
   const UI=state.localUI[contName] || {};
   const loading= UI.status==='loading';
   const finished= UI.status==='finished';
+  const shareObj= UI.shareObj||{};
   const {pageView,employee,customers:customers_}=state;
   const customers=(function(){
     if(!customers_) return [];
@@ -16,13 +17,31 @@ const mapStateToProps = (state) => {
   })();
 
   return {
-    pageView,employee,customers, UI, status:{loading,finished}
+    pageView,employee,customers, shareObj,UI, status:{loading,finished}
   }
 }
 const mapDispatchToProps = (dispatch) => {
+  const dispatchUI=cmd=>dispatch(updateUI({...cmd,contName}))
+  const getShare=params=>{
+      const {e:{target:{value}},ele:{GlobalEmpNbr:id},theShareObj}=params
+      const intValue=value|0;
+      const item={[id]:intValue};
+      const shareObj=theShareObj||{};
+      return mergeClone(shareObj,item)
+  }
+  const sum=obj=>Object.entries(obj)
+  .map(ele=>ele[1])
+  .reduce((acc,next)=> ~~acc+~~next)
   return {
-    updateUI:cmd=>dispatch(updateUI({...cmd,contName})),
-    selectEmp:id=>dispatch(selectEmp(id))
+    updateUI:cmd=>dispatchUI(cmd),
+    selectEmp:id=>dispatch(selectEmp(id)),
+    updateShare:params=>{
+      const shareObj=getShare(params);
+      const total=sum(shareObj);
+      dispatchUI({shareObj})
+      if(total!==100) return dispatchUI({warning:true,confirm:false,message:`Total is ${total}%,it must be 100%`});
+      if(total===100) return dispatchUI({warning:false,confirm:true});
+    },
   }
 }
 
