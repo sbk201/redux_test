4bf71e4f4aa964bfb04ae2f69b0f55cf8e4c598b
jsiu <john,siu@cookmedical.com>
2018-03-22 17:25:37 +0800

    small fix

--- src/components/Allocate.js
+++ src/components/Allocate.js
@@ -6,9 +6,6 @@ class Allocate extends Component {
 	componentDidMount() {
 		this.props.updateUI({warning:false,confirm:false});
   	}
-  	componentDidUpdate(){
-
-  	}
 
 	render(){
 	  	const {pageView,employee,customers,selectEmp,shareObj,UI,updateUI,updateShare,updateShare2}=this.props;
@@ -23,8 +20,31 @@ class Allocate extends Component {
 			}
 			return <Button {...attr}/>
 		})
-		const isHide=bool=>bool&&{style:{display:'none'}};
 		const selectedEmp=employee.filter(ele=>ele.selected);
+		const shareList=(function() {
+			const list=selectedEmp.map((ele,i)=>{
+	    		const attrs={
+	    			value:ele.value,
+	    			onChange: e=>{
+	    				const value=e.target.value|0;
+	    				const {GlobalEmpNbr}=ele;
+	    				updateShare2({GlobalEmpNbr,value})
+	    			},
+	    		}
+	    		const employeeGrid=(
+				<Segment key={i} style={{flexGrow:0}}>
+					<span>
+						<Icon link name='cancel' onClick={()=>selectEmp(ele.GlobalEmpNbr)}/>{ele.GlobalEmpName}
+					</span>
+    				<Input labelPosition='right' type='text' style={{float:'right'}}>
+    					<Label basic>%</Label> <input {...attrs}/> 
+				 	</Input>
+				</Segment> )
+	    		return employeeGrid
+			})
+			return list
+		})();
+		const isHide=bool=>bool&&{style:{display:'none'}};
 		const totalShare=(function() {
 			return selectedEmp.map(ele=>ele.value)
 			.reduce((acc,next)=>~~acc + ~~next , 0);
@@ -36,7 +56,7 @@ class Allocate extends Component {
 			// const warning= totalShare!==100;
 			const confirm= totalShare===100;
 			const message= (<Message warning {...messageStyle}> <p>Total is {totalShare}%,it must be 100%</p> </Message>);
-			const confirmBtn=(<Button content="confirm" style={{width:"100%"}}/>);
+			const confirmBtn=(<Button content="Submit" style={{width:"100%"}}/>);
 			return confirm? confirmBtn : message ;
 			return <div style={{height:'4em'}}></div>
 		})();
@@ -46,7 +66,7 @@ class Allocate extends Component {
 			<Segment inverted color="blue" size="large">
 	  			<h2>Customers list</h2>
 			    <Grid>
-			    	{customers.map((ele,i)=><Grid.Row key={i}><Grid.Column>{ele.GlobalCustName}</Grid.Column></Grid.Row>)}
+			    	{customers.map((ele,i)=><Grid.Row key={i}><Grid.Column>{ele.GlobalCustName} ({ele.globalCustNbr})</Grid.Column></Grid.Row>)}
 				</Grid>
 			</Segment>
 			<Segment inverted color="blue" size="large">
@@ -61,26 +81,7 @@ class Allocate extends Component {
 						</Grid.Column>
 						<Grid.Column width={8}>
 							<h3 style={{flexGrow:0}}>Employee Share</h3>
-						  	{selectedEmp.map((ele,i)=>{
-				    		const attrs={
-				    			value:ele.value,
-				    			onChange: e=>{
-				    				const value=e.target.value|0;
-				    				const {GlobalEmpNbr}=ele;
-				    				updateShare2({GlobalEmpNbr,value})
-				    			},
-				    		}
-				    		const employeeGrid=(
-		    				<Segment key={i} style={{flexGrow:0}}>
-		    					<span>
-		    						<Icon link name='cancel' onClick={()=>selectEmp(ele.GlobalEmpNbr)}/>{ele.GlobalEmpName}
-		    					</span>
-			    				<Input labelPosition='right' type='text' style={{float:'right'}}>
-			    					<Label basic>%</Label> <input {...attrs}/> 
-		    				 	</Input>
-		    				</Segment> )
-				    		return employeeGrid
-		    				})}
+						  	{shareList}
 		    				<div style={{height:"4em"}}>{messageShow}</div>
 	    				</Grid.Column>
 					</Grid.Row>
