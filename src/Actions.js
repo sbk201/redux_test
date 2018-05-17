6de3b8bce1b17c0e65473aef0b22f41f534746df
jsiu <john,siu@cookmedical.com>
2018-05-17 15:33:37 +0800

    simplify

--- src/Actions.js
+++ src/Actions.js
@@ -3,81 +3,26 @@ import {isTest,dummyData} from './init/global';
 
 export const updateUI=cmd=>({type: "UPDATE_UI", ...cmd});
 const receiveSbus=sbus=>({type: "RECEIVE_SBUS", sbus });
-const receiveCountries=countries=>({type: "RECEIVE_COUNTRIES", countries });
-const receiveCustomers=customers=>({type: "RECEIVE_CUSTOMERS", customers });
-const receiveContact=contact=>({type: "RECEIVE_CONTACT", contact });
-const receiveEmployee=employee=>({type: "RECEIVE_EMPLOYEE", employee });
-const receiveGcnCustomers=customers=>({type: "RECEIVE_GCN_CUSTOMERS", customers });
-export const selectCust=globalCustNbr=>({type: "SELECT_CUST", globalCustNbr});
-export const selectEmp=globalEmpNbr=>({type: "SELECT_EMP", globalEmpNbr});
-export const pickedSbu=sbu=>({type: "PICKED_SBU", sbu });
-export const pickedCountry=country=> ( {type: "PICKED_COUNTRY", country });
-export const checkShare=params=> ( {type: "CHECK_SHARE", ...params });
-export const nextView=view=>({type: "NEXT_VIEW", view });
 const link={
 	old:"http://localhost:5000/data",
-	sbu:"http://localhost:5000/hospAllocation/getSbu",
-	country:"http://localhost:5000/custAllocation/country",
-	customer:"http://localhost:5000/custAllocation/assigned_cust",
-	unCustomer:"http://localhost:5000/custAllocation/unassigned_cust",
-	contact:"http://localhost:5000/custAllocation/contact",
-	contact_cust:"http://localhost:5000/custAllocation/contact_cust",
-	sbu_employee:"http://localhost:5000/custAllocation/sbu_employee",
-	add_allocation:"http://localhost:5000/custAllocation/add_allocation",
-	delete_allocation:"http://localhost:5000/custAllocation/delete_allocation",
-	getNoGcnsOrNameCustDetail:"http://localhost:5000/custAllocation/getNoGcnsOrNameCustDetail",
-	getSbu:"http://localhost:5000/hospAllocation/getSbu",
-	getRep:"http://localhost:5000/hospAllocation/getRep",
-	getRep2:"http://10.65.1.24/appPortal/hospitalAllocation/lib/api.php?sbu_id=1396&limit=allocation&type=get_rep",
+	sbu:"http://localhost:5000/hospAllocation/sbu",
+	rep:"http://localhost:5000/hospAllocation/rep",
+	getRep:"http://10.65.1.24/appPortal/hospitalAllocation/lib/api.php?sbu_id=1396&limit=allocation&type=get_rep",
+}
+
+const api=async (method,item,params={})=>{
+	const link2= "http://localhost:5000/hospAllocation/"+item;
+	if(method==='get') return (await axios.get(link2,{params})).data
+	if(method==='post') return (await axios.post(link2,params)).data
+	if(method==='delete') return (await axios.delete(link2,{params})).data
 }
 
 const getSbusApi=async () => (await axios.get(link.sbu,{})).data;
-const getRepApi=async params => (await axios.get(link.getRep,{params})).data;
-const getCountriesApi=async () => (await axios.get(link.country,{})).data;
-const getUnassignedCustomerApi=async params => (await axios.get(link.unCustomer,{params})).data;
-const getAssignedCustomerApi=async params => (await axios.get(link.customer,{params})).data;
-const getContactApi=async params => (await axios.get(link.contact,{params})).data;
-const getContactCustApi=async params => (await axios.get(link.contact_cust,{params})).data;
-const getEmployeeApi=async params => (await axios.get(link.sbu_employee,{params})).data;
-const deleteAllocationApi=async params=> (await axios.get(link.delete_allocation,{params})).data;
-const addAllocationApi=async params=> (await axios.post(link.add_allocation,params)).data;
-const getNoGcnsOrNameCustDetailApi=async params=> (await axios.get(link.getNoGcnsOrNameCustDetail,{params})).data;
-const fetchGetCustomers=_params=>{
-	const {method,...params}=_params;
-	const apiFun={
-		contact:getContactApi,
-		contact_cust:getContactCustApi,
-		customer:getAssignedCustomerApi,
-		unassigned:getUnassignedCustomerApi,
-	}[method]
-	return async dispatch => {
-		const dispatchUI= cmd=>{
-			dispatch(updateUI({contName:'CustomerList',...cmd}))
-		};
-		dispatchUI({status:'loading'});
-
-		const result= await apiFun(params);
-		method==='contact' ?
-		dispatch(receiveContact(result)) : dispatch(receiveCustomers(result));
-		if(isTest && method!=='contact') dispatch(receiveCustomers(dummyData.customers.concat(result)));
-		dispatchUI({status:'finished',method});
-		// console.log('fetch',result);
-	}
-}
-
-const fetchGetEmployee=params=>{
-	const {sbu,country}=params;
-	return async dispatch => {
-		const employee=await getEmployeeApi({sbu,country});
-		dispatch(receiveEmployee(employee));
-		if(isTest) dispatch(receiveEmployee(dummyData.employee.concat(employee)));
-	}
-}
+const getRepApi=async params => (await axios.get(link.rep,{params})).data;
 export const smart= {
-	fetchGetCustomers,
 	fetchHome:(customers)=>{
 		return async dispatch=>{
-			const sbus=await getSbusApi();
+			const sbus=await api("get","sbu");
 			console.log('got result >>>',sbus)
 			dispatch(receiveSbus(sbus));
 		}
@@ -90,41 +35,4 @@ export const smart= {
 			}
 		}
 	},
-	fetchAdmin:(customers)=>{
-		return async dispatch=>{
-			const custDetail=await getNoGcnsOrNameCustDetailApi({customers});
-			dispatch(receiveGcnCustomers(custDetail));
-		}
-	},
-	fetchMain:()=>{
-		return async dispatch => {
-			const dispatchUI= cmd=>dispatch(updateUI({contName:'Main',...cmd}));
-			dispatchUI({status:'loading'});
-			const sbus=await getSbusApi();
-			const countries=await getCountriesApi();
-			dispatch(receiveSbus(sbus));
-			dispatch(receiveCountries(countries));
-			dispatchUI({status:'finished'});
-		}
-	},
-	editShare: params=>{
-		return async dispatch => {
-			const {customer,selectedEmp,sbuid}=params;
-			const globalCustNbr=customer.map(ele=>ele.globalCustNbr);
-			const deleteFn=()=> deleteAllocationApi({globalCustNbr,sbuid});
-			const addFn=()=>addAllocationApi({employee:selectedEmp,customerNbr:globalCustNbr,sbuID:sbuid});
-			console.log('selected Emp',selectedEmp)
-			const delResult=await deleteFn();
-			console.log('deleted',...delResult);
-			const addResult=await addFn();
-			console.log('added',...addResult);
-		}
-	},
-	afterSearchView:params=>{
-		const {sbu,country}=params;
-		return async dispatch => {
-			dispatch(fetchGetCustomers(params));
-			dispatch(fetchGetEmployee({sbu,country}));
-		}
-	}
 }
