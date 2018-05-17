2591dbd94d93a801f2eaf84a912edf8774ddae94
jsiu <john,siu@cookmedical.com>
2018-05-17 16:10:22 +0800

    temp

--- src/Actions.js
+++ src/Actions.js
@@ -3,24 +3,15 @@ import {isTest,dummyData} from './init/global';
 
 export const updateUI=cmd=>({type: "UPDATE_UI", ...cmd});
 const receiveSbus=sbus=>({type: "RECEIVE_SBUS", sbus });
-const link={
-	old:"http://localhost:5000/data",
-	sbu:"http://localhost:5000/hospAllocation/sbu",
-	rep:"http://localhost:5000/hospAllocation/rep",
-	getRep:"http://10.65.1.24/appPortal/hospitalAllocation/lib/api.php?sbu_id=1396&limit=allocation&type=get_rep",
-}
 
 const api=async (method,item,params={})=>{
-	const link2= "http://localhost:5000/hospAllocation/"+item;
-	if(method==='get') return (await axios.get(link2,{params})).data
-	if(method==='post') return (await axios.post(link2,params)).data
-	if(method==='delete') return (await axios.delete(link2,{params})).data
+	const link= "http://localhost:5000/hospAllocation/"+item;
+	if(method==='get') return (await axios.get(link,{params})).data
+	if(method==='post') return (await axios.post(link,params)).data
+	if(method==='delete') return (await axios.delete(link,{params})).data
 }
-
-const getSbusApi=async () => (await axios.get(link.sbu,{})).data;
-const getRepApi=async params => (await axios.get(link.rep,{params})).data;
 export const smart= {
-	fetchHome:(customers)=>{
+	fetchHome:()=>{
 		return async dispatch=>{
 			const sbus=await api("get","sbu");
 			console.log('got result >>>',sbus)
@@ -30,7 +21,7 @@ export const smart= {
 	getHospList:({method,...rest})=>{
 		return async dispatch => {
 			if(method==='rep') {
-				const rep=await getRepApi(rest);
+				const rep=await api("get","rep",rest);
 				console.log(rep);
 			}
 		}
