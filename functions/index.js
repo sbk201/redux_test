d5a4c3b964605d5fa03ce9d7ba1e723504b25371
sbk201 <pp00p@yahoo.com.hk>
2018-07-16 19:30:59 +0800

    deploy cloud function addMessage

--- functions/index.js
+++ functions/index.js
@@ -9,3 +9,19 @@ const functions = require('firebase-functions');
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 admin.initializeApp();
+
+exports.addMessage = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  return admin.firestore().collection('testItem').add({original: original}).then((snapshot) => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    return res.send(snapshot)
+  });
+});
+exports.log = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  console.log('got ',original);
+  res.send('done');
+});
