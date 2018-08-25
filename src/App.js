7cfbdf945e640b6ed91b172549789934610c8cce
sbk201 <pp00p@yahoo.com.hk>
2018-08-25 18:55:31 +0800

    init bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { connect } from "react-redux";
 import { smart } from "./Actions.js";
 import React, { Component } from "react";
 import "react-dom";
+import { Col,Row,Grid } from "react-bootstrap";
 
 const mapStateToProps = (state) => {
 	return { };
@@ -10,20 +11,56 @@ const mapDispatchToProps = (dispatch) => {
 	return {
 		fetch:()=>dispatch(smart.getIdeas()),
 	};
-}
+};
 class App extends Component {
 	componentDidMount() {
 		this.props.fetch();
 	}
+	// <div className="row">
+	// <div className="col-xs-6" style={{backgroundColor:"lightblue"}}>Part A</div>					
+	// <div className="col-xs-6" style={{backgroundColor:"green"}}>Part B</div>		
+	// </div>
 	render(){
 		return (
-			<div>
+			<div className="container-fluid">
 				<h1>Main App</h1>
-				<div className="container-fluid row">
-					<div className="col-xs-6" style={{backgroundColor:"lightblue"}}>Part A</div>					
-					<div className="col-xs-6" style={{backgroundColor:"green"}}>Part B</div>		
-				</div>
+				<Grid>
+					<Row className="show-grid">
+						<Col xs={12} md={8}>
+							<code>{"<Col xs={12} md={8} />"};</code>
+						</Col>
+						<Col xs={6} md={4}>
+							<code>{"<Col xs={6} md={4} />"}</code>
+						</Col>
+					</Row>
 
+					<Row className="show-grid">
+						<Col xs={6} md={4}>
+							<code>{"<Col xs={6} md={4} />"}</code>
+						</Col>
+						<Col xs={6} md={4}>
+							<code>{"<Col xs={6} md={4} />"}</code>
+						</Col>
+						<Col xsHidden md={4}>
+							<code>{"<Col xsHidden md={4} />"}</code>
+						</Col>
+					</Row>
+
+					<Row className="show-grid">
+						<Col xs={6} xsOffset={6}>
+							<code>{"<Col xs={6} xsOffset={6} />"}</code>
+						</Col>
+					</Row>
+
+					<Row className="show-grid">
+						<Col md={6} mdPush={6}>
+							<code>{"<Col md={6} mdPush={6} />"}</code>
+						</Col>
+						<Col md={6} mdPull={6}>
+							<code>{"<Col md={6} mdPull={6} />"}</code>
+						</Col>
+					</Row>
+				</Grid>
 			</div>
 		);
 	}
