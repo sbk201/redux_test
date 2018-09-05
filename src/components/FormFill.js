c570d213eda0ed5c8b600576ae857441027762c8
jsiu <john.siu@cookmedical.com>
2018-09-05 17:23:59 +0800

    fixed auto lost focus

--- src/components/FormFill.js
+++ src/components/FormFill.js
@@ -3,52 +3,61 @@ import { FormGroup,ControlLabel,FormControl,HelpBlock } from "react-bootstrap";
 import {defaults} from "lodash";
 // import PropTypes from "prop-types";
 
-const formConfig=({UI,updateUI})=>[{
+const _getConfig=(id,{UI,updateUI})=>[{
 	id:"someText",
 	validationState:"success",
+	label:"some text",
 	value:UI.value1||"" ,
 	onChange:e=>updateUI({value1:e.target.value})
-}];
-const InputField_=props=>({ _id, label, help, hide,...miniProps_ }) =>{
-	if(hide) return <div></div>;
-	const id="fromControl"+upperCase(_id);
-	const miniProps=defaults(miniProps_,{type:"text"});
-	const getConfig=id=>formConfig(props).find(ele=>ele.id===id);
-	// const validationState='success'
-	// const value="test value";
-	// const onChange=e=>console.log(e.target.value);
-	// return {validationState,value,onChange}
-	
-	const {validationState,value,onChange}=getConfig(_id);
+}]
+.find(ele=>ele.id===id);
+const InputField=params =>{
+	const { id, label, help, validationState="",...props }=params;
 	return (
-		<FormGroup controlId={id} {...{validationState}}>
+		<FormGroup controlId={id} validationState={validationState}>
 			<ControlLabel>{label}</ControlLabel>
-			<FormControl {...{...miniProps,value,onChange}} />
+			<FormControl {...props} />
 			{help && <HelpBlock>{help}</HelpBlock>}
-		</FormGroup>
-	);
+		</FormGroup>)
 };
 const getProps=props=>{
 	const {UI}=props;
-	const formValues=defaults(UI.formValues,{value:"empty value"});
-	const InputField=InputField_(props);
-	return {formValues,InputField};
+	const formValues=defaults(UI.formValues);
+	const getConfig=id=>_getConfig(id,props);
+	return {formValues,getConfig};
 };
 const upperCase=string=>string.charAt(0).toUpperCase() + string.substr(1);
 const FormFill=props=>{
   	const {UI,updateUI}=props;
-  	const {formValues,InputField}=getProps(props);
+  	const {getConfig}=getProps(props);
   	console.log(UI);
-
+  	
 	return (
 		<form>
-			<InputField
-				_id="someText"
-				label="Name"
-				placeholder="Enter text"
-				{...formValues}
-			/>
+			<InputField {...getConfig("someText")} />
 		</form>
 	);
 };
 export default FormFill;
+
+{/*			<InputField
+				_id="someText"
+				label="Name"
+				placeholder="Enter text"
+				{...formValues}
+			/>*/}
+
+
+			{/*<FormGroup
+	          controlId="formBasicText"
+	        >
+	          <ControlLabel>Working example with validation</ControlLabel>
+	          <FormControl
+	            type="text"
+	            value={UI.text2||""}
+	            placeholder="Enter text"
+	            onChange={e=>updateUI({text2:e.target.value})}
+	          />
+	          <FormControl.Feedback />
+	          <HelpBlock>Validation is based on string length.</HelpBlock>
+	        </FormGroup>*/}
