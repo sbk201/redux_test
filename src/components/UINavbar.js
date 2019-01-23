c9dd2f6482d1c66a20fe964dc1027d90bd2e7c21
sbk201 <pp00p@yahoo.com.hk>
2019-01-23 16:23:41 +0800

    navbar,auto close on click

--- src/components/UINavbar.js
+++ src/components/UINavbar.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { Component, Fragment as Frag} from "react";
+import { connect } from "react-redux";
 import { Nav,NavItem,Navbar } from "react-bootstrap";
+import {updateUI} from '../Actions.js'
 import { Link } from "react-router-dom";
+import "react-dom";
+const contName="UINavbar";
 
-const UINavbar=props=>{
-	const NavIt=({link,children})=><NavItem eventKey={1} componentClass={Link} href={link} to={link}>{children}</NavItem>
-	return (
-		<Navbar collapseOnSelect fixedTop>
+const mapStateToProps = (state) => {
+  const UI=state.localUI[contName] || {};
+	return { UI};
+};
+const mapDispatchToProps = (dispatch) => {
+  const dispatchUI=cmd=>dispatch(updateUI({...cmd,contName}));
+	return {
+    	updateUI:cmd=>dispatchUI({...cmd,contName}),
+	};
+};
+class UINavbar extends Component {
+	render(){
+		const expanded= this.props.UI.expanded;
+		const clickExpanded= ()=> this.props.updateUI({expanded:!expanded});
+		const NavIt=({link,children})=><NavItem eventKey={1} componentClass={Link} href={link} to={link}>{children}</NavItem>
+		return (
+		<Navbar fixedTop onToggle={clickExpanded} expanded={expanded}>
 			<Navbar.Header>
 				<Navbar.Brand style={{paddingTop:"0.5rem",paddingBottom:"0.5rem"}}>
 					<Link to="/" style={{padding:"0.5rem"}}>
@@ -16,7 +33,7 @@ const UINavbar=props=>{
 
 				<Navbar.Toggle />
 			</Navbar.Header>
-			<Navbar.Collapse>
+			<Navbar.Collapse onClick={clickExpanded}>
 				<Nav>
 					<NavIt link="/gallery"> Gallery </NavIt>
 					<NavIt link="/123123"> 404 </NavIt>
@@ -32,5 +49,10 @@ const UINavbar=props=>{
 			</Navbar.Collapse>
 		</Navbar>
 	);
-};
-export default UINavbar;
+	}
+}
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(UINavbar);
