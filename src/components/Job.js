45dc8b182e2242e1d21a11da0f2190369a98596d
sbk201 <pp00p@yahoo.com.hk>
2019-02-09 23:26:54 +0800

    toggle input for editing

--- src/components/Job.js
+++ src/components/Job.js
@@ -6,19 +6,24 @@ import {pipe, prop, map, addIndex} from "ramda";
 import { Link } from "react-router-dom";
 
 const getProps=props=>{
-	// const {updateJob}= props;
+	const {UI,updateUI}= props;
+	const { editing } = UI;
 	const mapIndex=addIndex(map);
-	const oneItem= ([title,value],index)=> <tr key={index}><td>{title}</td><td>{value}</td></tr>
+	const oneItem= ([title,value_],index)=> {
+		const value = editing ? <input defaultValue={value_} /> : value_;
+		return <tr key={index}><td>{title}</td><td>{value}</td></tr>
+	}
 	const MyTable= pipe(
 		prop('thisJob'),
 		Object.entries,
 		mapIndex(oneItem),
 		dom=> <table><tbody>{dom}</tbody></table>
 	)
-	return {MyTable};
+	const toggleEdit=()=>updateUI({editing:!editing});
+	return { MyTable, toggleEdit, editing};
 };
 const Jobs=props=>{
-  	const {MyTable}=getProps( props );
+	const { MyTable, toggleEdit, editing} = getProps(props);
   	const {thisJob}= props;
   	console.log(thisJob)
 	// const onEnter= fn=> e=> e.keyCode === 13 && fn( e.target.value );
@@ -27,6 +32,7 @@ const Jobs=props=>{
 			<Link to="/jobs"> <button>Back to List</button></Link>
 			<h1>Just Job</h1>
 			<MyTable thisJob={thisJob} />
+			<button onClick={toggleEdit}>Edit</button>
 		</div>
 	);
 };
