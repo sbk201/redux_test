7809dbf50aa445f3921b29a85b74f1222806fb92
jsiu <john.siu@cookmedical.com>
2019-02-11 16:34:08 +0800

    jobs,view button

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -3,25 +3,38 @@ import React from "react";
 // import {mapProp} from "../init/global";
 import {map, addIndex} from "ramda";
 import BootstrapTable from 'react-bootstrap-table-next';
-import cellEditFactory from 'react-bootstrap-table2-editor';
+// import cellEditFactory from 'react-bootstrap-table2-editor';
+import { createBrowserHistory } from 'history';
 
 const getProps=props=>{
 	const {updateJob}= props;
+	const rowEvents = {
+	  onClick: (e, row, rowIndex) => {
+	  	const className=e.target.className;
+	  	const id= row.id;
+	  	if(className==="theButton") props.history.push(`/jobs/${id}`);
+	  	// console.log(, row, rowIndex)
+		
+	  }
+	};
+	const buttonStyle={textAlign:"center", cursor: "pointer"};
 	const columns = [
 		{dataField: 'title', text: 'Title', sort: true},
-		{dataField: 'salary', text: 'Salary', sort: true},
+		{dataField: 'salary', text: 'Salary($)', sort: true},
 		// {dataField: 'totalHours', text: 'Total Hours', sort: true},
 		// {dataField: 'preDay', text: 'Pre Day($)', sort: true},
 		{dataField: 'preHour', text: 'Pre Hour($)', sort: true},
 		{dataField: 'url', text: 'URL'},
+		{dataField: 'button', text: '', style: buttonStyle, classes:"theButton"},
 	];
 	// const afterSaveCell= (o, n, job) => updateJob(job);
   	// const cellEdit=cellEditFactory({ mode: 'click', blurToSave:true, afterSaveCell});
-	return {columns};
+	return {columns, rowEvents};
 };
 const Jobs=props=>{
-  	const {columns}=getProps( props );
+  	const {columns, rowEvents}=getProps( props );
   	const {jobs}= props;
+  	const norm= map(job=> ({...job, button:"View"}))
 	const defaultSorted = [{
 	  dataField: 'preHour',
 	  order: 'desc'
@@ -30,7 +43,7 @@ const Jobs=props=>{
 	return (
 		<div> 
 			<h1>Jobs</h1>
-			<BootstrapTable keyField='id' data={ jobs } columns={ columns } defaultSorted={defaultSorted}
+			<BootstrapTable keyField='id' data={ norm(jobs) } columns={ columns } rowEvents={rowEvents} defaultSorted={defaultSorted}
  			striped hover condensed/>
 		</div>
 	);
